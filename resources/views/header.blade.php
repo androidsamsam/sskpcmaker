9d87df2b4a004c09314cdab7d3163a53e3480de2
androidsamsam <androidsamsam@gmail.com>
2024-09-14 17:21:23 +0900

    ヘッダー変更

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
     <div class=prheader>
 <P>人気ゲーミングPCを</P>
 <p>ブランドの枠を超えて一括で検索</P>
-<p>対象ブランドはマウスコンピューター、HP(OMNE、Victus)、FRONTIER、Alienware、GALLERIA</P>
-<p>モデル名のメモアイコンから、特徴などの解説を確認できます。</p>
+<p>対象ブランドはマウスコンピューター、HP(OMNE、Victus)、FRONTIER、GALLERIA、パソコン工房</P>
+<!--<p>モデル名のメモアイコンから、特徴などの解説を確認できます。</p> -->
 <p>ただいま、工事中</p>
     </div>
